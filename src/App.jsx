617b7bb5f402f692e04136b2849d2a41ccc490cb
mdaz78 <mdabuzafar78@gmail.com>
2025-12-30 11:23:55 +0530

    feat: enhance Menu component with data loading and error handling
    - Updated Menu component to utilize a loader for fetching menu data from the API.
    - Integrated error handling in routing for the Menu and AppLayout components.
    - Added Loader component to indicate loading state during navigation.
    - Improved error display in the Error component to show relevant error messages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Cart from './features/cart/Cart';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
 import AppLayout from './ui/AppLayout';
+import ErrorElement from './ui/Error';
 import Home from './ui/Home';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <ErrorElement />,
     children: [
       {
         path: '/',
@@ -18,6 +20,8 @@ const router = createBrowserRouter([
       {
         path: '/menu',
         element: <Menu />,
+        loader: menuLoader,
+        errorElement: <ErrorElement />,
       },
       {
         path: '/cart',
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import CartOverview from '../features/cart/CartOverview';
 import Header from './Header';
+import Loader from './Loader';
 
-const AppLayout = ({ children }) => {
+const AppLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
+  console.log({ navigation });
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
