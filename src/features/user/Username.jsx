a0cf7f8cc0d20423c17349a7cc5cee30283c757b
mdaz78 <mdabuzafar78@gmail.com>
2026-01-12 11:22:39 +0530

    style: enhance UI components with Tailwind CSS adjustments
    - Updated CartOverview for better layout and spacing.
    - Improved CreateUser form styling for better user experience.
    - Modified Username component to hide on smaller screens.
    - Refined AppLayout and Header for improved structure and responsiveness.
    - Enhanced Home component to include CreateUser and adjusted layout.

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Username = () => {
-  return <div className="text-sm font-semibold">Zafar</div>;
+  return <div className="text-sm font-semibold hidden md:block">Zafar</div>;
 };
 
 export default Username;
