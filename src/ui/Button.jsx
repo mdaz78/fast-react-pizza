fb9b30da61b90145b84b1cf539256a1babe414b8
mdaz78 <mdabuzafar78@gmail.com>
2026-01-12 18:37:23 +0530

    style: enhance CreateOrder component layout and input styling
    - Updated CreateOrder component to improve layout with Tailwind CSS classes for better spacing and alignment.
    - Refined input fields with consistent styling and added error message handling.
    - Adjusted Button component styles for better visual consistency across the application.

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, type }) {
   const base =
-    'inline-block cursor-pointer rounded-full bg-yellow-500 font-semibold tracking-wide uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed';
+    'inline-block text-sm cursor-pointer rounded-full bg-yellow-500 font-semibold tracking-wide uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed';
 
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
     secondary:
-      'inline-block cursor-pointer rounded-full font-semibold tracking-wide uppercase transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:ring focus:ring-stone-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed border-stone-300 border-2 px-4 py-3 md:px-6 md:py-3.5 text-stone-400 hover:text-stone-800',
+      'inline-block text-sm cursor-pointer rounded-full font-semibold tracking-wide uppercase transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:ring focus:ring-stone-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed border-stone-300 border-2 px-4 py-3 md:px-6 md:py-3.5 text-stone-400 hover:text-stone-800',
   };
 
   if (to) {
